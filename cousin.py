ebd616848a609ae2c4eab5d843424d1631f841a0
Rodevp <rodriguezrodrigor12@gmail.com>
2021-01-30 16:20:56 -0500

    agregando metodo de factores

--- cousin.py
+++ cousin.py
@@ -6,3 +6,26 @@ def convert_to_integers( list_of_numbers ) :
         list_numbers_integers.append( number )
     
     return list_numbers_integers
+
+
+#muestra los posibles divisores entre sus factores
+def all_you_factors(number) :
+    factor = 1
+    list_factors_number = []
+    limit = number
+
+    while limit != 1 :
+        if limit % factor == 0 :
+
+            list_factors_number.append(factor)
+            limit = limit / factor 
+            factor = 1
+        
+        factor += 1
+    
+    #si no encuentra el factor como si mismo lo agrega
+    if not number in list_factors_number : list_factors_number.append(number)
+
+    return list_factors_number
+
+
